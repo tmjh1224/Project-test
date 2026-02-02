4eec0e98b80883782826a5c686ff49c67ccd5265
Irving <tmjh1224@gmail.com>
2026-02-02 13:36:37 +0800

    TQC+ 程式語言Python 410 繪製等腰三角形

--- a.py
+++ a.py
@@ -1,4 +1,9 @@
 a = int(input())
-
 for i in range(1,a+1):
-    print(i)
+    for j in range(1,a+i):
+        if(j <= a-i or j >= a+i ):
+            print(" ",end="")
+        else:
+            print("*",end="")
+    print()
+#TQC+ 程式語言Python 410 繪製等腰三角形
